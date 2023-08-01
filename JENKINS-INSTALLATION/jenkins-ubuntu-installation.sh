44a05be06fef1da663ebbfd41365354c89954b94
NdumLab <98489709+NdumLab@users.noreply.github.com>
2023-07-31 23:12:27 -0400

    Update jenkins-ubuntu-installation.sh
    Update jenkins-ubuntu-installation.sh

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -1,18 +1,48 @@
+# Update your system
+sudo apt update -y
+sudo apt upgrade -y
 
-# Jenkins Installation And Setup In AWS EC2 ubuntu Instance.
-# Installation of Java
-sudo apt update   # Update the repositories
-sudo apt install openjdk-11-jdk
-java -version
-# Install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo dnf upgrade
-# Add required dependencies for the jenkins package
-sudo dnf install chkconfig java-devel
-sudo dnf install jenkins
-# Start Jenkins
-sudo systemctl daemon-reload  # To Register the Jenkins service 
+# Add the Jenkins repository to your system:
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update your package lists and install Jenkins:
+sudo apt update -y
+sudo apt install jenkins -y
+
+#Install java
+sudo apt install openjdk-11-jre-headless -y
+
+# Start Jenkins:
 sudo systemctl start jenkins
-systemctl status jenkins
+
+# Enable Jenkins
+sudo systemctl enable jenkins
+
+# Check the status of Jenkins:
+sudo systemctl status jenkins
+================================================
+    1  sudo apt update
+    2  sudo apt upgrade
+    3  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    4  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+    5  sudo apt update
+    6  sudo apt install jenkins
+    7  sudo journalctl -xe
+    8  sleep 5
+    9  sudo service jenkins start
+   10  sudo reboot
+   11  sudo service jenkins start
+   12  sudo journalctl -xe
+   13  java --version
+   14  sudo apt remove jenkins
+   15  sudo apt install openjdk-11-jre-headless
+   16  sudo apt install jenkins
+   17  java --version
+   18  sudo service jenkins start
+   19  sudo service jenkins eneble
+   20  sudo service jenkins status
+   21  history
